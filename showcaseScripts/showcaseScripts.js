c8fab45efb482fb9707190b1580f7a444154adae
NatePrograms <56306794+NatePrograms@users.noreply.github.com>
2020-04-20 11:56:47 -0400

    Adding Page
    Adding my page and needed materials

---
+++ showcaseScripts/showcaseScripts.js
@@ -0,0 +1,7 @@
+/*let showcasePyFighter = document.createElement('showcasePyFighter')
+let showcasePyFighterText = document.createTextNode('<h1>Text</h1>')
+showcasePyFighter.appendChild(showcasePyFighterText)
+let webPage = document.getElementById('webPage')
+webPage.appendChild(showcasePyFighter)*/
+document.getElementById("showcasePyFighter").innerHTML = "<h1>This content is testing, testing, testing</h1>"
+document.getElementById("showcasePyFighterVideo").innerHTML = "<video src='video.mp4' controls width='400', height ='200' ></video>"
