6ceaa87488c4d20ef25aed687a29f85161bad237
NatePrograms <56306794+NatePrograms@users.noreply.github.com>
2020-04-22 09:29:45 -0400

    Finished Page
    I finished the page that was assigned to me and added all necessary materials

--- showcaseScripts/showcaseScripts.js
+++ showcaseScripts/showcaseScripts.js
@@ -1,7 +1,12 @@
-/*let showcasePyFighter = document.createElement('showcasePyFighter')
-let showcasePyFighterText = document.createTextNode('<h1>Text</h1>')
-showcasePyFighter.appendChild(showcasePyFighterText)
-let webPage = document.getElementById('webPage')
-webPage.appendChild(showcasePyFighter)*/
-document.getElementById("showcasePyFighter").innerHTML = "<h1>This content is testing, testing, testing</h1>"
-document.getElementById("showcasePyFighterVideo").innerHTML = "<video src='video.mp4' controls width='400', height ='200' ></video>"
+
+document.getElementById("showcasePyFighter").innerHTML = "<h1>PyFighter Showcase</h1> <br> <p1>The following will show the different basic parts of the PyFighter game in pictures, then a video explaining the project in greater detail</p1>"
+
+document.getElementById("showcasePyFighterPictures").innerHTML = "<br><img src='Picture1.png' width='286', height='331'></img><br><img src='Picture2.png' width='395' height='123'></img><br><img src='Picture3.png' width='433' height='497'></img>"
+
+document.getElementById("showcasePyFighterContent").innerHTML = "<br><p1>The pictures above show 3 basic points to the PyFighter game. The first shows the player class, including only its basic stats and healthbar drawing. The stats are all used in later parts. The second shows the second player, codenamed baddy, as it has all values of Player except for color and name. Finally, the last pictures show how some controls were created and how they are used</p1>"
+
+document.getElementById("showcasePyFighterVideo").innerHTML = "<br><video src='showcasePyFighter.mp4' controls width='400', height ='200' ></video>"
+
+document.getElementById("showcaseWebPage").innerHTML = "<br><h1>WebPageShowcase</h1><br><p1>The following video will show off this code. Yay</p1>"
+
+document.getElementById("showcaseWebPageVideo").innerHTML = "<br><video src='showcaseWebPage.mp4' controls width='400', height ='200' ></video>"
